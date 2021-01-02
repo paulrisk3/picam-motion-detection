ea38a9c6d8fad2895f3873c56499c3c2eb5ca204
Paul Risk <paulrisk3@gmail.com>
2021-01-01 20:27:26 -0500

    Records motion clips.

--- opencv-server.py
+++ opencv-server.py
@@ -1,6 +1,7 @@
 import cv2
 import datetime
 import sys
+import os
 import time
 
 while True:
@@ -9,18 +10,22 @@ while True:
         # This status thing just gets bigger and bigger. What's the purpose of keeping track of it outside of the immediate cycle?
         status_list=[None, None]
         video=cv2.VideoCapture("http://192.168.1.126:8000/stream.mjpg")
+        record_length = 10
+        size = (int(video.get(3)), int(video.get(4)))
         baseline_counter=0
 
         while True:
             check, frame = video.read()
-            status=0
+            motion_status=0
             gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
             gray_frame=cv2.GaussianBlur(gray_frame,(25,25),0)
 
+            # Reset the baseline_counter every 300 frames
             if baseline_counter == 300:
                 baseline_counter=0
             baseline_counter = baseline_counter+1
 
+            # Reset baseline_image to the current gray_frame after every 300 frames
             if baseline_counter==1:
                 baseline_image=gray_frame
                 continue
@@ -32,16 +37,26 @@ while True:
             for contour in contours:
                 if cv2.contourArea(contour) < 10000:
                     continue
-                status=1
+                motion_status=1
                 (x, y, w, h)=cv2.boundingRect(contour)
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 1)
-            status_list.append(status)
+            status_list.append(motion_status)
 
+            # If motion is detected, record for record_length seconds
             if status_list[-1]==1 and status_list[-2]==0:
-                print("Motion detected at " + str(datetime.datetime.now()))
+                now = datetime.datetime.now()
+                print("Motion detected at " + str(now))
+                clip_directory = 'clips/' + now.strftime('%Y') + '/' + now.strftime('%m') + '/' + now.strftime('%d') + '/'
+                if not os.path.exists(clip_directory):
+                    os.makedirs(clip_directory)
+                video_clip = cv2.VideoWriter(clip_directory + now.strftime("%Y-%m-%d_%H-%M-%S") + '.avi', cv2.VideoWriter_fourcc(*'MJPG'), 30, size)
+                time_end = record_length + time.time()
+                while time.time() < time_end:
+                    check, frame = video.read()
+                    video_clip.write(frame)
 
         video.release()
     except:
-        # print(sys.exc_info()[0])
+        video.release()
         print("Retrying connection in 10 seconds...")
         time.sleep(10)
